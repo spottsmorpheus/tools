7a467e427c229d5c85dab892f75ffb3cb53faea6
Morpheus Steve Potts <morpheus@Morpheuss-MacBook-Pro.local>
2022-02-23 15:17:18 +0000

    Updated moprheusvars

--- groovy/morpheusvars.groovy
+++ groovy/morpheusvars.groovy
@@ -1,28 +1,47 @@
 import static groovy.json.JsonOutput.*
 
-if (customOptions) {
-    println("==== customOptions =========")
-    print(prettyPrint(toJson(customOptions)))
-} else {
-    println("No CustomOptions variable defined")
-}
-if (instance) {
-    println("==== instance ==============")
-    print(prettyPrint(toJson(instance)))
-} else {
-    println("No instance variable defined")
+try {
+    if (customOptions) {
+        println("==== customOptions =========")
+        println(prettyPrint(toJson(customOptions)))
+    } else {
+        println("No CustomOptions variable defined")
+    }
+} catch(ex) {
+    println("customOptions Exception ${ex.message}")
 }
 
-if (server) {
-    println("==== server ================")
-    print(prettyPrint(toJson(server)))
-} else {
-    println("No server variable defined")
+
+try {
+    if (instance) {
+        println("==== instance ==============")
+        println(prettyPrint(toJson(instance)))
+    } else {
+        println("No instance variable defined")
+    }
+} catch(ex) {
+    println("instance Exception ${ex.message}")
 }
 
-if (zone) {
-    println("==== zone ==================")
-    print(prettyPrint(toJson(zone)))
-} else {
-    println("No zone variable defined")
+
+try {
+    if (server) {
+        println("==== server ================")
+        println(prettyPrint(toJson(server)))
+    } else {
+        println("No server variable defined")
+    }
+} catch(ex) {
+    println("server Exception ${ex.message}")
+}
+
+try {
+    if (zone) {
+        println("==== zone ==================")
+        println(prettyPrint(toJson(zone)))
+    } else {
+        println("No zone variable defined")
+    }
+} catch(ex) {
+    println("zone Exception ${ex.message}")
 }
