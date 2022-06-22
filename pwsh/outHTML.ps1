21d2ae17cf14fc9797dc21119811016fcbce924c
Morpheus Steve Potts <morpheus@Morpheuss-MacBook-Pro.local>
2022-06-22 08:23:57 +0100

    outHtml Updates

--- pwsh/outHTML.ps1
+++ pwsh/outHTML.ps1
@@ -11,22 +11,25 @@ body {
     font-size:1.0em;
 	line-height:1.0em;
 	font-weight:normal;
-	background-color:white;}
+	background-color:#white}
 table {
     font-family: Arial, Helvetica, sans-serif;
     border-collapse: collapse;
+    background-color:#fcfcfc;
     width: 100%;}
 td, th {
     border: 1px solid #ddd;
     white-space: nowrap;
     padding: 5px;}
+tr:nth-child(even) {
+    background-color: #d1d1d1}
 tr:hover {
-    background-color: #ddd;}
+    background-color: #3db5e6;}    
 th {
     padding-top: 12px;
     padding-bottom: 12px;
     text-align: left;
-    background-color: #253746;
+    background-color: #185a7d;
     color: white;
   }
 .logentry {
@@ -78,10 +81,9 @@ Function Out-HtmlPage {
         [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
         [Object[]]$InputObject,
         [String]$Title,
-        [String]$Filename="/tmp/myfile.html",
         [String]$Style=$Script:HtmlStyle,
         [Hashtable]$ClassMap=$Script:ClassMap,
-        [Object]$Footer="morpheusdata.com"
+        [Object]$Footer="MorpheusData.com"
     )
 
     Begin {
@@ -112,8 +114,8 @@ Function Out-HtmlPage {
         [void]$html.AppendLine('</body>')
         [void]$html.AppendLine('</html>')
         #Generate the HTML Report
-        $html.ToString() | Set-Content -Path $Filename
-        invoke-item $Filename
+        $html.ToString() | Set-Content -Path "/tmp/myfile.html"
+        invoke-item "/tmp/myfile.html"
     }
 }
 
@@ -146,7 +148,7 @@ Function Make-HTMLTable {
     [void]$html.Append('<thead>')
     [void]$html.Append('<tr>')
     $Headers | Foreach-Object {[void]$Html.AppendFormat('<th>{0}</th>',$_)}
-    [void]$html.Append('<tr>').AppendLine()
+    [void]$html.Append('</tr>').AppendLine()
     [void]$html.AppendLine('</thead>')
     [void]$html.AppendLine('<tbody>')
 
@@ -163,7 +165,7 @@ Function Make-HTMLTable {
                 [void]$html.AppendFormat('<td>{0}</td>',$cell)
             }
         }
-        [void]$html.AppendLine('<tr>') 
+        [void]$html.AppendLine('</tr>') 
     }
     [void]$html.AppendLine('</tbody>')
     [void]$html.AppendLine('</table>')
